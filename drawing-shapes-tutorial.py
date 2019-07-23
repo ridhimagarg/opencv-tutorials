e5a4780955f86688a98a3dac3137b0d618e18e93
Ridhima Garg <garg.ridhima72@gmail.com>
2019-07-23 14:57:09 +0530

    shapes update

--- drawing-shapes-tutorial.py
+++ drawing-shapes-tutorial.py
@@ -1,3 +1,8 @@
+'''
+Here will see how to use shapes features of opencv to be use
+in different application.
+'''
+
 import cv2
 
 
@@ -19,6 +24,7 @@ cv2.rectangle(img, (0,0), (265,185), (0,255,0), 3)
 cv2.imshow('rectangle', img)
 cv2.waitKey(0)
 
+## Putting some text in image
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(img, 'House', (130,160) ,font, 0.5, (255,180,0), 1, cv2.LINE_AA )
 cv2.imshow('text', img)
