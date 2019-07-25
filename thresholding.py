8c0ef1de4c988827f783c1be57e4a1576c850bcd
Ridhima Garg <garg.ridhima72@gmail.com>
2019-07-25 13:34:54 +0530

    thresholding update

--- thresholding.py
+++ thresholding.py
@@ -1,4 +1,5 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 img = cv2.imread('images/image8.jpg')
@@ -12,8 +13,16 @@ ret5, thresh5 = cv2.threshold(img, 127, 255, cv2.THRESH_TOZERO_INV)
 images = [thresh1, thresh2, thresh3, thresh4, thresh5]
 titles = ['Binary', 'Binary inverse', 'Truncate', 'Tozero', 'Tozero inverse']
 
+## Showing up using opencv
 for i in range(5):
     cv2.imshow(titles[i], images[i])
     cv2.waitKey(0)
-)
 
+
+## Showing up using matplotlib
+for i in range(5):
+    plt.subplot(2,3,(i+1))
+    plt.imshow(images[i],'gray')
+    plt.xlabel(titles[i])
+
+plt.show()
