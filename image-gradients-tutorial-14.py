eacefb04c57bb0a021427becef9face4598317b2
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-25 01:02:34 +0530

    gradient image

---
+++ image-gradients-tutorial-14.py
@@ -0,0 +1,12 @@
+'''
+Application of gardients in images 
+
+Gradients here itself means partial derivatives.
+Gradients are useful in detecting the edges
+'''
+
+import cv2
+import numpy as numpy
+import matplotlib.pyplot as plt
+
+    
