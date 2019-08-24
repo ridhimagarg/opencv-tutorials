49880fc4ae3f578086bc6232e4bc0cbe0134f9b1
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-25 01:09:25 +0530

    gradient image explanation

--- image-gradients-tutorial-14.py
+++ image-gradients-tutorial-14.py
@@ -2,7 +2,14 @@
 Application of gardients in images 
 
 Gradients here itself means partial derivatives.
-Gradients are useful in detecting the edges
+Gradients are useful in detecting the edges based on color 
+gradients.
+
+Derivative of a matrix is calculated by an operator called Laplacian.
+
+For derivatives, we have to cal. two derivatives, Sobal derivatives - one is horizontal, vertical.
+
+This is some mathematical details, let's go to implementation.
 '''
 
 import cv2
