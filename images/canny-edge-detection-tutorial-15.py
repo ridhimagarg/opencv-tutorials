5536850a2e40a45a53b166d9dfa3a0865dfa9c95
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-29 18:33:58 +0530

    canny edges

--- images/canny-edge-detection-tutorial-15.py
+++ images/canny-edge-detection-tutorial-15.py
@@ -7,8 +7,14 @@ This is into 4 phases
 
 Noise Reduction -> Finding gradient direction and magnitude -> Non-max supression -> Hystresis Thresholding.
 
+In Noise reduction, remove noise using smoothing filters like gaussian.
 
+Finding gradient magnitude -> Using first derivative i.e, Sobel filters will find the direction (angle between horizontal and vertical derivative)
+And magnitude using both horiz and vetical  = np.sqrt(Sx**2 + Sy**2)
 
+Non-max supression -> Basically a first stage filtering for non-edges.
+
+Hystersis thresholding -> Using range for maxval and minval, filter edges and non-edges
 '''
 
 import cv2
