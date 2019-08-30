c2e6f35436edc5da18b18fb6c7f73d73a5b69396
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-30 12:41:09 +0530

    draw contours precisely

--- contours1-tutorial-16.py
+++ contours1-tutorial-16.py
@@ -15,25 +15,17 @@ img = cv2.imread('images/gradients3.png')
 
 imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow('gray', imgray)
-cv2.waitKey(0)
+## Applying gaussian smoothing
+blur = cv2.GaussianBlur(imgray, ksize = (5, 5), sigmaX = 0)
 
-blur = cv2.GaussianBlur(src = imgray, 
-    ksize = (5, 5), 
-    sigmaX = 0)
-(t, binary) = cv2.threshold(src = blur,
-    thresh = 200, 
-    maxval = 255, 
-    type = cv2.THRESH_BINARY)
+## Applying thresholding of binary type.
+(t, binary) = cv2.threshold(blur, thresh = 200, maxval = 255, type = cv2.THRESH_BINARY)
 
-
-#ret,thresh = cv2.threshold(imgray,127,255,0)
-
+## Finding the contours
 contours, hierarchy =  cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+## Drawing these contours
 img = cv2.drawContours(img, contours, -1, (0,255,0), 3)
-
-#contours, hierarchy = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 
 cv2.imshow('contours',img)
 cv2.waitKey(0)
