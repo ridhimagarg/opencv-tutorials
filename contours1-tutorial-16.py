34697f9a98379d439657f4e5f7380af52494ac67
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-30 11:31:40 +0530

    contours

--- contours1-tutorial-16.py
+++ contours1-tutorial-16.py
@@ -1,3 +1,16 @@
+'''
+
+Contours are a curve joining all the continous points having same intensity(color).
+
+Performing the find contour, try to convert image into binary.
+
+
+'''
+
 import cv2
 import matplotlib.pyplot as pyplot
 import numpy as np
+
+img = cv2.imread('images/gradients.jpg')
+
+imgray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
