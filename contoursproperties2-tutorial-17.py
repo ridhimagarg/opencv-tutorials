3050727358a90a6ed1eefb6be4b28f68e1e83711
Ridhima Garg <garg.ridhima72@gmail.com>
2019-08-30 14:57:39 +0530

    contours properties

--- contoursproperties2-tutorial-17.py
+++ contoursproperties2-tutorial-17.py
@@ -2,4 +2,15 @@ import cv2
 import matplotlib.pyplot as pyplot
 import numpy as np
 
-img = cv2.imread('images/gradients3.png')
+img = cv2.imread('images/gradients3.png',0)
+
+(t, binary) = cv2.threshold(img, thresh = 200, maxval = 255, type = cv2.THRESH_BINARY)
+
+## Finding the contours
+contours, hierarchy =  cv2.findContours(binary, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+cnt = contours[0]
+
+M = cv2.moments(cnt)
+print(M)
+
