480fb47e7197349d5e5ea6447f209b046ba57120
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-02 15:53:13 +0530

    contour approximation

--- contoursproperties3-tutorial-18.py
+++ contoursproperties3-tutorial-18.py
@@ -10,15 +10,17 @@ cv2.waitKey(0)
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #ret,thresh = cv2.threshold(imgray,127,255,0)
 
-## Finding the contours
-contours, hierarchy =  cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+canny = cv2.Canny(img,100,200)
 
-cv2.drawContours(img, contours, -1, (0,255,0), 3)
+## Finding the contours
+contours, hierarchy =  cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+
+#cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
 ## Finding centroid of first contour only
 cnt = contours[0]
 
-epsilon = cv2.arcLength(cnt,True) 
+epsilon = 0.1*cv2.arcLength(cnt,True) 
 approx = cv2.approxPolyDP(cnt,epsilon,True)
 
 cv2.drawContours(img,approx,-1,(0,255,0),3)
