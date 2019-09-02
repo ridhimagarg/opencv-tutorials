895cc5b0a34f1ea79fbbd440d05fa4d3ddccf52a
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-02 14:37:56 +0530

    sudoko cropping new block using centroid

--- contoursproperties2-tutorial-17.py
+++ contoursproperties2-tutorial-17.py
@@ -4,8 +4,6 @@ import numpy as np
 
 img = cv2.imread('images/gradients3.png')
 
-#(t, binary) = cv2.threshold(img, thresh = 200, maxval = 255, type = cv2.THRESH_BINARY)
-
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 #ret,thresh = cv2.threshold(imgray,127,255,0)
 
@@ -14,23 +12,25 @@ contours, hierarchy =  cv2.findContours(imgray, cv2.RETR_TREE, cv2.CHAIN_APPROX_
 
 cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
-print(np.array(contours).shape)
-
+## Finding centroid of first contour only
 cnt = contours[0]
 
+## moment of that contour
 M = cv2.moments(cnt)
 print(M)
 
+## Calculating centroids coordinates
 cx = int(M['m10']/M['m00'])
 cy = int(M['m01']/M['m00'])
 
 print(cx, cy)
 
-## Let's use these calculated centroid values
+## Let's use these calculated centroid values to get some new image out of original
 
 cv2.imshow('Original', img)
 
-
+## Calculating new coordinates for new image 
+## Trying to extract the middle portion of it.
 x_axis = cx/2
 y_axis = cy/2
 
@@ -40,11 +40,14 @@ end_x = int(start_x + cx)
 start_y = int(cy/2)
 end_y = int(start_y + cy)
 
-print(start_x, start_y, end_x, end_y)
+## Coordinates for new image
+print("Starting coordinates and ending coordinates are ",start_x, start_y, end_x, end_y)
 
-
+## Cropping new image from original image
 new_img = img[start_y:end_y, start_x:end_x]
-print(img.shape)
+
 
 cv2.imshow('new',new_img)
 cv2.waitKey(0)
+
+## Here we are trying to extract the middle sudoko box but due to uneveness of full image, we are not able to detect the correct box, let's see how to change orientation in order to ger better results.
