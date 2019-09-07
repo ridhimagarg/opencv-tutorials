df01de9ba86798206f1803d75d3b63e37c9e6665
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-07 20:14:35 +0530

    AR and extent

--- contoursproperties4-tutorials-19.py
+++ contoursproperties4-tutorials-19.py
@@ -1,12 +1,24 @@
 '''
 Going to discuss some advanced properties.
+
+Application Resource -> https://gurus.pyimagesearch.com/lesson-sample-advanced-contour-properties/
+
 How to use solidity property to distingush between 'X' and '0' in tictactoe image
+
+1. Aspect Ratio -> width/height
+2. Extent -> Object Area/ Bounding Rect area
+
+Aspect ratio(AR) can be used to detect shape like square and circle most probably have AR as 1.
+Extent can aslo be used for shape detection.
 
 '''
 
 import cv2
 import numpy as np 
 import matplotlib.pyplot as plt 
+
+
+##------------------------------------------ First Application --------------------------##
 
 img = cv2.imread('images/contours_tictactoe.png')
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
@@ -59,3 +71,46 @@ for (i, c) in enumerate(cnts):
 cv2.imshow('Output',img)
 cv2.waitKey(0)
 #print(cnts[0])
+
+
+
+
+##-------------------------------- Second Application ---------------------------##
+
+img = cv2.imread('images/contours_tetris_blocks.png')
+imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+thresh = cv2.threshold(imgray, 225, 255, cv2.THRESH_BINARY_INV)
+
+cnts, heir = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+hullimage = np.zeros(imgray.shape[:2], dtype="uint8")
+
+for (i, c) in enumerate(cnts):
+	# compute the area of the contour along with the bounding box
+	# to compute the aspect ratio
+	area = cv2.contourArea(c)
+	(x, y, w, h) = cv2.boundingRect(c)
+ 
+	# compute the aspect ratio of the contour, which is simply the width
+	# divided by the height of the bounding box
+	aspectRatio = w / float(h)
+ 
+	# use the area of the contour and the bounding box area to compute
+	# the extent
+	extent = area / float(w * h)
+ 
+	# compute the convex hull of the contour, then use the area of the
+	# original contour and the area of the convex hull to compute the
+	# solidity
+	hull = cv2.convexHull(c)
+	hullarea = cv2.contourArea(hull)
+	solidity = area / float(hullarea)
+ 
+	# visualize the original contours and the convex hull and initialize
+	# the name of the shape
+	cv2.drawContours(hullimage, [hull], -1, 255, -1)
+	cv2.drawContours(img, [c], -1, (240, 0, 159), 3)
+	shape = ""
+
+
