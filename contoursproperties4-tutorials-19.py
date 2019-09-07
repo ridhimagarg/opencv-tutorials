b34564b5a3cfa9246ad2256ec14a8d1fee4f2b76
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-07 19:35:50 +0530

    using solidity and area finding x and 0 in tictac

--- contoursproperties4-tutorials-19.py
+++ contoursproperties4-tutorials-19.py
@@ -17,10 +17,10 @@ cv2.waitKey(0)
 
 cnts, heir = cv2.findContours(imgray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
-cv2.drawContours(img,cnts,-1,(0,255,0),3)
+# cv2.drawContours(img,cnts,-1,(0,255,0),3)
 
-cv2.imshow('Contours',img)
-cv2.waitKey(0)
+# cv2.imshow('Contours',img)
+# cv2.waitKey(0)
 
 for (i, c) in enumerate(cnts):
     #print(i,c)
@@ -32,4 +32,19 @@ for (i, c) in enumerate(cnts):
     hullarea = cv2.contourArea(hull)
     solidity = area/float(hullarea)
 
+    char = '?'
+
+    if solidity > 0.8:
+        char = '0'
+    
+    elif solidity > 0.5:
+        char = 'X'
+
+    if char != '?':
+        cv2.drawContours(img, [c], -1, (0,255,0), 3)
+        cv2.putText(img, char, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 255, 0), 4)
+
+
+cv2.imshow('Output',img)
+cv2.waitKey(0)
 #print(cnts[0])
