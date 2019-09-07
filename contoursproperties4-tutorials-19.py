57207eb53801ae607cd6bd68d58e633e43d9393c
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-07 19:23:17 +0530

    solidity

--- contoursproperties4-tutorials-19.py
+++ contoursproperties4-tutorials-19.py
@@ -15,7 +15,21 @@ cv2.imshow('Original', img)
 cv2.imshow('Grayed',imgray)
 cv2.waitKey(0)
 
-cnts = cv2.findContours(imgray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts, heir = cv2.findContours(imgray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+cv2.drawContours(img,cnts,-1,(0,255,0),3)
+
+cv2.imshow('Contours',img)
+cv2.waitKey(0)
 
 for (i, c) in enumerate(cnts):
-    print(i,c)
+    #print(i,c)
+    area = cv2.contourArea(c)
+    (x,y,w,h) = cv2.boundingRect(c)
+    print(i, area)
+
+    hull = cv2.convexHull(c)
+    hullarea = cv2.contourArea(hull)
+    solidity = area/float(hullarea)
+
+#print(cnts[0])
