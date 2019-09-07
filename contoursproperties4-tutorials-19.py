2984406fb61b2c2beeaca4e1e34b767935628f43
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-07 19:58:28 +0530

    using solidity and area finding x and 0 in tictac

--- contoursproperties4-tutorials-19.py
+++ contoursproperties4-tutorials-19.py
@@ -1,5 +1,7 @@
 '''
-Going to discuss some advanced properties
+Going to discuss some advanced properties.
+How to use solidity property to distingush between 'X' and '0' in tictactoe image
+
 '''
 
 import cv2
@@ -11,11 +13,16 @@ imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
 print(img.shape,imgray.shape)
 
+## Showing up Original and gray image
 cv2.imshow('Original', img)
 cv2.imshow('Grayed',imgray)
 cv2.waitKey(0)
 
+## First finding the contours
 cnts, heir = cv2.findContours(imgray.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+## You can uncomment it if you want to see the full list of contours
+
 
 # cv2.drawContours(img,cnts,-1,(0,255,0),3)
 
@@ -31,15 +38,19 @@ for (i, c) in enumerate(cnts):
     hull = cv2.convexHull(c)
     hullarea = cv2.contourArea(hull)
     solidity = area/float(hullarea)
+    print(solidity)
 
     char = '?'
 
+    ## solidity value: 0.8 for "0" because it has less concexity defect 
     if solidity > 0.8:
         char = '0'
-    
+
+    ## solidity cvalue: 0.5 for "X" as it has higher convexity so less solidity value.   
     elif solidity > 0.5:
         char = 'X'
 
+    ## Drawing contour and put text of detected shape
     if char != '?':
         cv2.drawContours(img, [c], -1, (0,255,0), 3)
         cv2.putText(img, char, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.25, (0, 255, 0), 4)
