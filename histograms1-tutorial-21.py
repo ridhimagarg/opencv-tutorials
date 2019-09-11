17aac1d2a082dc4aa6a24094b8004f0975f1c930
Ridhima Garg <garg.ridhima72@gmail.com>
2019-09-11 17:52:26 +0530

    histogram plotting

--- histograms1-tutorial-21.py
+++ histograms1-tutorial-21.py
@@ -4,8 +4,16 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('images/contours_hierarchy.png')
 
+## here [0] means gray scale image
+## If colored image, pass [0], [1], [2] for b,g,r
 hist = cv2.calcHist([img],[0],None,[256],[0,256])
 
 print(hist)
 
 print(hist.shape)
+
+## Plotting histograms
+plt.hist(img.ravel(), 256, [0,256])
+
+## In this picture we have only black and white pixels so 0(black) and 255(white-bright) has high(all) numbers
+plt.show()
