89c9e35f7b835bdcefea0e5818218fe69186603e
Ridhima Garg <garg.ridhima72@gmail.com>
2019-10-20 19:36:05 +0530

    template matching try all

--- template-matching1.py
+++ template-matching1.py
@@ -1,6 +1,6 @@
 import numpy as np 
 import cv2
-import matplotlib.pyplot as pyplot
+import matplotlib.pyplot as plt
 
 img = cv2.imread('images/messi.jpg', 0)
 img2 = img.copy()
@@ -23,5 +23,21 @@ for meth in methods:
 
     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
 
-    
+    # If the method is TM_SQDIFF or TM_SQDIFF_NORMED, take minimum
+    if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+        top_left = min_loc
+    else:
+        top_left = max_loc 
+    bottom_right = (top_left[0] + w, top_left[1] + h)
+
+    cv2.rectangle(img2, top_left, bottom_right, 255, 2)
+
+    plt.subplot(121), plt.imshow(res, cmap='gray')
+    plt.title('Matching result')
+    plt.subplot(122), plt.imshow(img, cmap='gray')
+    plt.title('Detected object')
+
+    plt.suptitle(meth)
+
+    plt.show()
 
