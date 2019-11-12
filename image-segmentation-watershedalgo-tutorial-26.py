368211b3f2bf8a67c89fac015640cddb666267a7
Ridhima Garg <garg.ridhima72@gmail.com>
2019-11-12 16:28:46 +0530

    watershed algo

--- image-segmentation-watershedalgo-tutorial-26.py
+++ image-segmentation-watershedalgo-tutorial-26.py
@@ -32,7 +32,7 @@ gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 cv2.imshow('image', gray)
 
 
-
+## Applying dilation for sure_bg detection
 ret, thresh = cv2.threshold(gray,0,255,cv2.THRESH_BINARY_INV+cv2.THRESH_OTSU)
 
 kernel = np.ones((3,3), np.uint8)
@@ -43,30 +43,35 @@ cv2.imshow('Open', opening)
 
 sure_bg = cv2.dilate(opening, kernel, iterations=3)
 
+## Sure background image
 cv2.imshow('dilated', sure_bg)
 
+## Cjecking what if simple threshlded image is dilated (Otional)
 bg = cv2.dilate(thresh, kernel, iterations=3)
 
 cv2.imshow('bg', bg)
 
+## Foreground extraction
 dist_transform = cv2.distanceTransform(opening,cv2.DIST_L2,5)
 ret, sure_fg = cv2.threshold(dist_transform,0.7*dist_transform.max(),255,0)
 
 
 sure_fg = np.uint8(sure_fg)
 
+## The unknown area is between sure background and sure foreground
 unknown = np.subtract(sure_bg, sure_fg)
 
 cv2.imshow('unknown', unknown)
 
-ret, markers = cv2.connectedComponents(sure_fg)
 
-#cv2.imshow('markers', markers)
+ret, markers = cv2.connectedComponents(sure_fg)
 
 print(markers)
 
+## Add one so that sure background is not 1
 markers = markers +1
 
+## Making the unknown area as 0
 markers[unknown == 255] = 0
 
 #cv2.imshow('markers2', markers)
@@ -75,6 +80,7 @@ cv2.waitKey(0)
 
 markers = cv2.watershed(img, markers)
 
+## boundary region is marked with -1
 img[markers == -1] = (255, 0, 0)
 
 cv2.imshow('watershed',img)
