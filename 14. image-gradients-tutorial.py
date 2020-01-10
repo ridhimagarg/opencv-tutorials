f38b412ca9380c39bd165cf594d25b6324ada371
Ridhima Garg <garg.ridhima72@gmail.com>
2020-01-10 14:55:00 +0530

    image gradients

--- 14. image-gradients-tutorial.py
+++ 14. image-gradients-tutorial.py
@@ -7,7 +7,7 @@ gradients.
 
 Derivative of a matrix is calculated by an operator called Laplacian.
 
-For derivatives, we have to cal. two derivatives, Sobal derivatives - one is horizontal, vertical.
+For derivatives, we have to calculate two derivatives, Sobal derivatives - one is horizontal, vertical.
 
 This is some mathematical details, let's go to implementation.
 '''
@@ -18,10 +18,14 @@ import matplotlib.pyplot as plt
 
 img = cv2.imread('images/gradients4.jpg',0)
 
+## Applying laplacian 
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
+
+## Applying sobel filters for x and y direction
 sobelx = cv2.Sobel(img, cv2.CV_64F, 1,0, ksize=5)
 sobely = cv2.Sobel(img, cv2.CV_64F, 0,1, ksize=5)
 
+## Showing up all the images
 images = [img, laplacian, sobelx, sobely]
 titles = ['Original', 'Laplacian', 'SobelX', 'SobelY']
 
@@ -32,6 +36,7 @@ for i in range(4):
 
 plt.show()
 
+## Applying filters on other iamges
 img = cv2.imread('images/gradients2.jpg',0)
 
 laplacian = cv2.Laplacian(img, cv2.CV_64F)
@@ -41,6 +46,7 @@ sobely = cv2.Sobel(img, cv2.CV_64F, 0,1, ksize=5)
 images = [img, laplacian, sobelx, sobely]
 titles = ['Original', 'Laplacian', 'SobelX', 'SobelY']
 
+## Plotting into two different ways
 for i in range(4):
     plt.subplot(2,3,(i+1))
     plt.imshow(images[i], cmap='gray')
